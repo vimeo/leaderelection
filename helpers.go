52b5e8239417a00b41d2d0ac3abc42f2cca03751
Sachi Nagada <sachi.nagada@vimeo.com>
2020-09-14 11:42:24 -0700

    Update comment for GetSelfHostPort and make more efficient

--- helpers.go
+++ helpers.go
@@ -5,29 +5,25 @@ import (
 	"net"
 )
 
-// GetSelfHostPort gets the global unicast address of where the application is
-// running and returns a slice of IP:Port which can be passed to Config.HostPort
+// GetSelfHostPort gets the global unicast addresses of the local execution
+// environment and returns a slice of IP:Port which can be passed to
+// Config.HostPort
 func GetSelfHostPort(port string) ([]string, error) {
 	addresses, addrErr := net.InterfaceAddrs()
 	if addrErr != nil {
 		return nil, fmt.Errorf("Unable to get self IP address: %w", addrErr)
 	}
 
-	var selfIP []string
+	selfIPs := make([]string, 0, 1)
 	for _, addr := range addresses {
-		var ip net.IP
 		switch v := addr.(type) {
 		case *net.IPNet:
-			ip = v.IP.To4()
-			if ip == nil {
-				ip = v.IP.To16()
+			if v.IP != nil && v.IP.IsGlobalUnicast() {
+				hostPort := net.JoinHostPort(v.IP.String(), port)
+				selfIPs = append(selfIPs, hostPort)
 			}
-		}
-		if ip != nil && ip.IsGlobalUnicast() {
-			hostPort := net.JoinHostPort(ip.String(), port)
-			selfIP = append(selfIP, hostPort)
 		}
 	}
 
-	return selfIP, nil
+	return selfIPs, nil
 }
